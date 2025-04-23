8b50ccd49e9e612164e637c7d754c4f761571cd5
NatJudd <nathaniel.judd25@ycdsbk12.ca>
2025-04-23 14:11:58 -0400

    started the meal class. added getters, abstract methods and a constructor

--- src/Meal.java
+++ src/Meal.java
@@ -4,5 +4,37 @@
  * @author NJudd
  */
 public abstract class Meal {
-    
+    private String name;
+    private String time;
+    private int calories;
+    private int foodGroups;
+
+    public Meal(String name, String time, int calories, int foodGroups) {
+        this.name = name;
+        this.time = time;
+        this.calories = calories;
+        this.foodGroups = foodGroups;
+    }
+
+    public abstract boolean isHealthy();
+
+    public abstract boolean isGoodTime();
+
+    public abstract String getSummary();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getFoodGroups() {
+        return foodGroups;
+    }
 }
