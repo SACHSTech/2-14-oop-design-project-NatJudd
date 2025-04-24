c172d1f2a6fad85a5459c6b7f4a305ef4d94eefc
NatJudd <nathaniel.judd25@ycdsbk12.ca>
2025-04-24 11:43:12 -0400

    added getSnackAmount() method

--- src/Snack.java
+++ src/Snack.java
@@ -6,9 +6,11 @@
 public class Snack extends Meal {
     private final int healthCalsMax = 200;
     private final int healthFoodGroups = 2;
+    private int amount;
 
     public Snack(String name, String time, int calories, int foodGroups) {
         super(name, time, calories, foodGroups);
+        amount += 1;
     }
 
     @Override
@@ -23,5 +25,14 @@ public class Snack extends Meal {
     @Override
     public String getMeal() {
         return "[" + getTime() + "] Snack: " + getName() + "(" + getCalories() + ")";
+    }
+
+    /**
+     * Returns the amount of snack objects a person ate
+     * 
+     * @return amount of snacks
+     */
+    public int getSnackAmount() {
+        return amount;
     }
 }
