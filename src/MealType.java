760dfa94a6dcd987037e1e6fd0b79d0aa2c47194
NatJudd <nathaniel.judd25@ycdsbk12.ca>
2025-05-01 15:27:05 -0400

    Created enums for mealTypes

---
+++ src/MealType.java
@@ -0,0 +1,26 @@
+public enum MealType {
+    BREAKFAST("breakfast"),
+    LUNCH("lunch"),
+    DINNER("dinner"),
+    SNACK("snack");
+
+    private final String name;
+
+    /**
+     * Constructor
+     * 
+     * @param name meal type display name
+     */
+    MealType(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the meal type display name
+     * 
+     * @return display name
+     */
+    public String getName() {
+        return name;
+    }
+}
